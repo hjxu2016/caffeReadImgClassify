55e52f9df9eee4969f896cba5162c71fcefbfa28
hjxu2016 <xhjfighting@139.com>
2018-10-23 10:22:09 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/caffeReadImgClassify.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/caffeReadImgClassify.dir/main.cpp.o"
+  "caffeReadImgClassify.pdb"
+  "caffeReadImgClassify"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/caffeReadImgClassify.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
